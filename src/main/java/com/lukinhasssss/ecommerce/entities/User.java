ba5ba6a30bc679a0037df92392e128e55cf6bc78
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-07-04 20:43:12 -0300

    Add entity User, user database seed

---
+++ src/main/java/com/lukinhasssss/ecommerce/entities/User.java
@@ -0,0 +1,39 @@
+package com.lukinhasssss.ecommerce.entities;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
